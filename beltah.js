ac9b5d92ba50fc8c2296b28b9195bd58d88d27cd
Beltah254 <devbeltah87@gmail.com>
2024-11-21 04:17:31 +0300

    Beltah Tech 254 🇰🇪

--- beltah.js
+++ beltah.js
@@ -120,9 +120,7 @@ setTimeout(() => {
     });
   }
 });
-        const zk = (0, baileys_1.default)(sockOptions);
-        store.bind(zk.ev);
-        setInterval(() => { store.writeToFile("store.json"); }, 3000);
+        
         zk.ev.on("messages.upsert", async (m) => {
             const { messages } = m;
             const ms = messages[0];
